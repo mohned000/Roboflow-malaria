feed067f456fc2f6f8a471ab34f136e0654511e8
mohned000 <156646075+mohned000@users.noreply.github.com>
2025-06-04 18:19:24 +0200

    Delete test directory

--- test/milaria_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:your_app_name/services/api.dart';
-
-void main() {
-  test('تحقق من استجابة الخادم', () async {
-    final result = await ApiService().analyzeImage('sample.jpg');
-    expect(result.containsKey('result'), true);
-  });
-}
